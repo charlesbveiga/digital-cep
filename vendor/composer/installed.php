6076d40de45433d0a7b59b7da55bdcba4c853f0e
Charles Batista Veiga <84667490+charlesbveiga@users.noreply.github.com>
2024-10-19 15:31:08 -0300

    digital-cep

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ubuntu/composer',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ubuntu/composer' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wead/digital-cep' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => '63b5624635c0c1e0f2020f672dcfd235dd9834ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wead/digital-cep',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
